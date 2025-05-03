9c6e4ff04a5cd250787e0ace8693fd40d5d30927
Antonio Chavez <antoniogiancarlos531@gmail.com>
2025-05-03 03:04:13 +0200

    Mantener estructura de templates

--- cuyzin/settings.py
+++ cuyzin/settings.py
@@ -50,7 +50,9 @@ ROOT_URLCONF = 'cuyzin.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'templates'),  # Agrega esta línea
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
