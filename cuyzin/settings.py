e9649e82df923c242e512157d6784b59c5650ae1
Antonio Chavez <antoniogiancarlos531@gmail.com>
2025-05-03 15:55:16 +0200

    base

--- cuyzin/settings.py
+++ cuyzin/settings.py
@@ -67,10 +67,10 @@ WSGI_APPLICATION = 'cuyzin.wsgi.application'
 
 # Database configuration
 DATABASES = {
-    'default': dj_database_url.config(
-        default=f"sqlite:///{os.path.join(BASE_DIR, 'db.sqlite3')}",
-        conn_max_age=600,
-    )
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
 
 # Password validation
