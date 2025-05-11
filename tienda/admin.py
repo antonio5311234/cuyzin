e77fb59d3cda0e75762d31450ec65649811e3e9f
Antonio Chavez <antoniogiancarlos531@gmail.com>
2025-05-12 01:13:23 +0200

    Tu mensaje antonio

--- tienda/admin.py
+++ tienda/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Ejercicio1, Ejercicio2
+
+@admin.register(Ejercicio1)
+class Ejercicio1Admin(admin.ModelAdmin):
+    list_display = ['nombre', 'Lieu', 'Cuisine', 'Spécialité', 'Prix_des_plats_principaux', 'Vue', 'Réservations']
+    search_fields = ['Lieu', 'Cuisine', 'Spécialité']
+
+@admin.register(Ejercicio2)
+class Ejercicio2Admin(admin.ModelAdmin):
+    list_display = ['nombre', 'Lieu', 'Cuisine', 'Spécialité', 'Prix_des_plats_principaux', 'Vue', 'Réservations']
+    search_fields = ['Lieu', 'Cuisine', 'Spécialité']
